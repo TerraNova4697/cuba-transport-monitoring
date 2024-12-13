0cf0d069aaef291cfb3b216b27eaa9801185890c
TerraNova4697 <nikitabechthold@gmail.com>
2024-12-13 12:50:18 +0500

    Chacnged daemon service

--- setup.sh
+++ setup.sh
@@ -4,5 +4,4 @@ sudo pip3 install -r requirements.txt
 sudo mv /cuba/cuba-transport-monitoring/cuba-transport-monitoring.service /lib/systemd/system/cuba-transport-monitoring.service
 sudo systemctl daemon-reload
 sudo systemctl enable cuba-transport-monitoring.service
-sudo systemctl start cuba-transport-monitoring.service
 sudo systemctl status cuba-transport-monitoring.service
